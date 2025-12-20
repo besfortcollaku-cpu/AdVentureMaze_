c304c7aafa6ed7e2bbff5c963f5014c0c4239fb3
Cotty <besfortcollaku@gmail.com>
2025-12-20 13:59:11 +0100

    force canvas render with boarder

--- frontend/maze-game/src/main.js
+++ frontend/maze-game/src/main.js
@@ -4,18 +4,20 @@ const app = document.querySelector('#app')
 
 app.innerHTML = `
     <h1>Adventure Maze</h1>
-    <canvas id="game" width="400" height="400"></canvas>
+    <canvas id="game" width="400" height="400"
+      style="border:2px solid black; background:#eee;">
+    </canvas>
 `
 
 const canvas = document.getElementById('game')
 const ctx = canvas.getContext('2d')
 
-let player = { x: 20, y: 20, size: 20 }
+const player = { x: 20, y: 20, size: 20 }
 
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height)
 
-      ctx.fillStyle = '#f5f5f5'
+      ctx.fillStyle = '#ddd'
         ctx.fillRect(0, 0, canvas.width, canvas.height)
 
           ctx.fillStyle = '#2c7be5'
@@ -23,4 +25,5 @@ function draw() {
           }
 
           draw()
-
+  `}
+}
