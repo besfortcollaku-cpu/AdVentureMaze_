32abd919546968d9cf3fa6c11335b0164cf507a1
besfortcollaku-cpu <besfort.collaku@gmail.com>
2025-12-25 20:00:44 +0100

    add levels

--- frontend/maze-game/src/levels/index.js
+++ frontend/maze-game/src/levels/index.js
@@ -4,6 +4,6 @@ import { level1 } from "./level1.js";
 import { level2 } from "./level2.js";
 import { level3 } from "./level3.js";
 import { level4 } from "./level4.js";
-
+import { level242 } from "./level242.js";
 // ✅ Order matters: Next Level uses this array
-export const levels = [level1, level2, level3, level4];
+export const levels = [level1, level2, level3, level4, level4];
