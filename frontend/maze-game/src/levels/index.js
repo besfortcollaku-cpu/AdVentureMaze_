3dc60966d8c8b30c7eb48a53222e70ff9e647898
besfortcollaku-cpu <besfort.collaku@gmail.com>
2025-12-25 14:06:03 +0100

    rollback4

--- frontend/maze-game/src/levels/index.js
+++ frontend/maze-game/src/levels/index.js
@@ -1,4 +1,6 @@
-import { level242 as level1 } from "./level1.js";
+// src/levels/index.js
+import { level1 } from "./level1.js";
 import { level2 } from "./level2.js";
 
+// ✅ Order matters (Next Level uses this array)
 export const levels = [level1, level2];
