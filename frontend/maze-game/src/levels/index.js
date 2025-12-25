2abb5dee648de2ad3a0f02b88d54e47fa8497cfc
besfortcollaku-cpu <besfort.collaku@gmail.com>
2025-12-25 17:15:33 +0100

    add levels

--- frontend/maze-game/src/levels/index.js
+++ frontend/maze-game/src/levels/index.js
@@ -5,7 +5,10 @@
 import { level242 as level1 } from "./level242.js"; // keep filename, but it's LEVEL 1 now
 
 import { level2 } from "./level2.js";
+import { level3 } from "./level3.js";
+import { level4 } from "./level4.js";
+import { level5 } from "./level5.js";
 
 
 
-export const levels = [level1, level2];
+export const levels = [level1, level2, level3, level4, level5];
