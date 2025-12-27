881ef77f8d77a3c5d00f1cb8b61547f40d8e59b2
besfortcollaku-cpu <besfort.collaku@gmail.com>
2025-12-27 18:42:15 +0100

    test

--- frontend/maze-game/src/levels/index.js
+++ frontend/maze-game/src/levels/index.js
@@ -5,7 +5,6 @@
 
 import { level242 as level1 } from "./level242.js"; // keep filename, but it's LEVEL 1 now
 import { level2 } from "./level2.js";
-import { level242 as level1 } from "./level242.js";
 import { level2 } from "./level2.js";
 
 export const levels = [level1, level2];
