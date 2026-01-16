899779e97707146d8d748397dcdaf49c4b758544
cottyPhone <besfort.collaku@gmail.com>
2026-01-16 16:42:44 +0100

    login fix

--- frontend/maze-game/src/main.js
+++ frontend/maze-game/src/main.js
@@ -189,7 +189,7 @@ document.getElementById("controls")?.addEventListener("click", () => {
   ui.showLevelSelect({
   totalLevels: levels.length,
   currentLevel: levelIndex + 1,
-  isCompleted: (lvl) => lvl < UNLOCKED_LEVEL,
+  isCompleted: (lvl) => lvl < UNLOCKED_LEVEL +1,
 });
 });
 
@@ -338,7 +338,7 @@ ui.onLevelSelect((selectedIndex) => {
 
   game.start();
 
-
+}
 // ---------------------------
 // Level flow
 // ---------------------------
