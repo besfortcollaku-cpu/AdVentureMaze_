297577d613c4d20d4378f5f403d58003da4a7a93
besfortcollaku-cpu <besfort.collaku@gmail.com>
2026-01-19 14:34:53 +0100

    level to Welcome

--- frontend/maze-game/src/ui/ui.js
+++ frontend/maze-game/src/ui/ui.js
@@ -619,9 +619,7 @@ function hideWelcome() {
   testBtn?.addEventListener("click", () => {
   hideWelcome();
 
-  // TEMP: open level select with dummy data
-  showLevelSelect({
-  });
+  
 });
 
   if (name !== "guest") {
