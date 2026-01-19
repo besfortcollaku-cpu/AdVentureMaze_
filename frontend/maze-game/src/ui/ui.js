55f3536f9a0d57d5ce6aee58fe4c3a20a7128845
besfortcollaku-cpu <besfort.collaku@gmail.com>
2026-01-19 14:42:40 +0100

    DIVS ARANGE

--- frontend/maze-game/src/ui/ui.js
+++ frontend/maze-game/src/ui/ui.js
@@ -4,23 +4,27 @@ export function mountUI(app) {
   app.innerHTML = `
     <div class="phone">
       <div class="topbar">
-      <div class="topRow">
-      <div class="brand">
-      <div class="logoBox" title="Adventure Maze">
-      <img src="/logo.png" alt="Adventure Maze Logo" />
-    </div>
-    </div>
-      <div class="levelWrap">
-      <div class="levelNew">NEW!</div>
-    <div class="levelText">Adventure Maze</div>
-    <div class="coins" title="Coins">
-      <div class="coinDot"></div>
-      <div id="coinCount">0</div>
-    </div>
-    </div>
-    <div class="iconRow">
-          ${iconBtn("settingsBtn", gearSVG(), "")}
-          ${iconBtn("controls", joystickSVG(), "")}
+        <div class="topRow">
+          <div class="brand">
+            <div class="logoBox" title="Adventure Maze">
+              <img src="/logo.png" alt="Adventure Maze Logo" />
+               <div class="levelWrap">
+                 <div class="levelNew">NEW!</div>
+                 <div class="levelText">Adventure Maze</div>
+               </div>
+               <div class="coins" title="Coins">
+                 <div class="coinDot"></div>
+                 <div id="coinCount">0</div>
+               </div>
+            </div>
+          </div>
+        
+        
+        
+      </div>
+      <div class="iconRow">
+        ${iconBtn("settingsBtn", gearSVG(), "")}
+        ${iconBtn("controls", joystickSVG(), "")}
     </div>
     </div>
     </div>
