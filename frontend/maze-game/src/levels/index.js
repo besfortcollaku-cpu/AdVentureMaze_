d61d8f368dd3edc36c129b9f708bca7f17f12777
besfortcollaku-cpu <besfort.collaku@gmail.com>
2026-01-19 17:33:03 +0100

    lvl 10

--- frontend/maze-game/src/levels/index.js
+++ frontend/maze-game/src/levels/index.js
@@ -11,9 +11,10 @@ import { level6 } from "./level6.js";
 import { level7 } from "./level7.js";
 import { level8 } from "./level8.js"; 
 import { level9 } from "./level9.js";
+import { level10 } from "./level10.js";
 
 
-export const levels = [level1, level2, level3,level4, level5, level6, level7,level8, level9];
+export const levels = [level1, level2, level3,level4, level5, level6, level7,level8,level9,level10];
 
 export function getLevel(index) {
   return levels[index] || null;
