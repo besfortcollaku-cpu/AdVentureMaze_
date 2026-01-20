94aa66a3f5ebf699a4403c0231ae4a9ad602830a
cottyPhone <besfort.collaku@gmail.com>
2026-01-20 21:53:31 +0100

    level screen

--- frontend/maze-game/src/main.js
+++ frontend/maze-game/src/main.js
@@ -26,7 +26,7 @@ let COINS = 0;
 
 // prevent double reward per completion
 let rewardedThisLevel = false;
-let UNLOCKED_LEVEL = 1;
+const UNLOCKED_LEVEL = 1;
 
 // ---------------------------
 // Backend helpers
@@ -241,8 +241,8 @@ ui.onLevelSelect((selectedIndex) => {
 const savedLevel = Number(serverProgress?.level || 1);
   levelIndex = clampLevelIndex(savedLevel - 1);
   UNLOCKED_LEVEL = savedLevel;
-  
-  
+ 
+  CURRENT_USER = { username: serverUser.username, uid: serverUser.uid };
 
   COINS = Number(serverUser.coins || 0);
   ui.setCoins(COINS);
