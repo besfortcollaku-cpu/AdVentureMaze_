8c1ecef2139654c5604527490fc0807b95a20d1b
cottyPhone <besfort.collaku@gmail.com>
2026-02-11 19:55:11 +0100

    settings

--- frontend/maze-game/src/game/movement.js
+++ frontend/maze-game/src/game/movement.js
@@ -1,7 +1,7 @@
 // src/game/movement.js
 
 import { ensureAudioUnlocked } from "./rollSound.js";
-import { getSettings, onSettingsChange } from "../settings.js";
+import { getSettings, onSettingsChange } from "./src/settings.js";
 import { startRollSound, updateRollSound, stopRollSound } from "./rollSound.js";
 
 let gyroEnabled = getSettings().gyro;
