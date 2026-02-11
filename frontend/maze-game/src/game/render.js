45c66ffd2262f027ca407d6b10e3ebff24b79015
cottyPhone <besfort.collaku@gmail.com>
2026-02-11 01:09:23 +0100

    sdi ma qkanu ba

--- frontend/maze-game/src/game/render.js
+++ frontend/maze-game/src/game/render.js
@@ -97,8 +97,8 @@ function drawWallShadow(px, py) {
   // ── WALL SHADOW (light from bottom-left → shadow top-right)
 ctx.fillStyle = "rgba(0,0,0,0.28)";
 ctx.fillRect(
-  px + tile * 0.2,   // → right
-  py - tile * 0.22,   // ↑ up
+  px + tile * 0.32,   // → right
+  py - tile * 0.32,   // ↑ up
   tile,
   tile
 );
