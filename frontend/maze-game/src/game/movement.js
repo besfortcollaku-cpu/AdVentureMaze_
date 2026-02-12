d809f926842c5208291dbc1cea2e58ebacce6c17
cottyPhone <besfort.collaku@gmail.com>
2026-02-12 10:17:56 +0100

    sound

--- frontend/maze-game/src/game/movement.js
+++ frontend/maze-game/src/game/movement.js
@@ -1,5 +1,8 @@
 // src/game/movement.js
 
+import { unlockAudio, startRollSound, stopRollSound } from "./rollSound.js";
+import { getSettings } from "../settings.js";
+
 import { unlockAudio } from "./rollSound.js";
 
 export function createMovement({ state, onMoveFinished }) {
