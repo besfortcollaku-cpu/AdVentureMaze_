4f6821b5623106a853daf6f07feae4511c3cef86
cottyPhone <besfort.collaku@gmail.com>
2026-02-14 03:33:09 +0100

    refresh done fixed

--- frontend/maze-game/src/main.js
+++ frontend/maze-game/src/main.js
@@ -139,7 +139,6 @@ ui.setCoins(me.user.coins ?? 0);
 CURRENT_USER.free_skips_used = me.user.free_skips_used ?? 0;
 CURRENT_USER.free_hints_used = me.user.free_hints_used ?? 0;
 CURRENT_USER.free_restarts_used = me.user.free_restarts_used ?? 0;
-updateRestartBadge();
 
 return me;
 }
@@ -517,7 +516,6 @@ restartPopup.onBuyRestart(async () => {
 };
   game.setLevel(levels[levelIndex]);
   restartPopup.hide();
-  updateRestartBadge();
 });
 
 restartPopup.onWatchAdRestart(async () => {
@@ -600,7 +598,6 @@ ui.onLoginClick(async () => {
   if (!game.isRunning?.()) {
       
     game.start();
-    updateRestartBadge();
 }
 });
 }
