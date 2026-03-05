dc07f606518df3df9feb121c27f77219276ecfae
Your Name <besfort.collaku@gmail.com>
2026-03-05 14:46:54 +0100

    hints

--- frontend/maze-game/src/hints/levelRoutes.js
+++ frontend/maze-game/src/hints/levelRoutes.js
@@ -4,7 +4,7 @@ export const LEVEL_ROUTES = {
   // Example:
   // 1: ["right", "up", "left", "down"],
 
-  11: ["right", "down", "left", "up"],
+  11: ["right", "down", "left", "up", "right", "down"],
 
   // Add more:
   // 12: ["right", "right", "up", "left", ...],
