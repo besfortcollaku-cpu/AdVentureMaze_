8d6f3aeddc11d34526a9297da87e4f84cd155387
Your Name <besfort.collaku@gmail.com>
2026-03-05 14:01:55 +0100

    hints

---
+++ frontend/maze-game/src/hints/levelRoutes.js
@@ -0,0 +1,11 @@
+// One route per level: array of directions the player should swipe
+// Directions allowed: "up", "down", "left", "right"
+export const LEVEL_ROUTES = {
+  // Example:
+  // 1: ["right", "up", "left", "down"],
+
+  11: ["up", "up", "up", "up"],
+
+  // Add more:
+  // 12: ["right", "right", "up", "left", ...],
+};
