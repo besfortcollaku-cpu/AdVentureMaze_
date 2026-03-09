cc27176f7f0b4c68abd0056a30f4b039c32e8ce3
Your Name <besfort.collaku@gmail.com>
2026-03-09 21:22:06 +0100

    missed day

--- frontend/maze-game/src/main.js
+++ frontend/maze-game/src/main.js
@@ -1946,7 +1946,6 @@ ui.onGuestStart(() => {
 // ---- PI LOGIN ----
 
 ui.onLoginClick(async (e) => {
-  // fix "first tap does nothing" + prevent double taps
   e?.preventDefault?.();
   e?.stopPropagation?.();
   if (LOGIN_IN_PROGRESS) return;
@@ -2039,31 +2038,28 @@ ui.onLoginClick(async (e) => {
     hideLoginLoading();
     updateAllBadges();
     LOGIN_IN_PROGRESS = false;
-    
-const meFresh = await apiMe();
-console.log("WHO OPENED DAILY POPUP", meFresh?.dailyReward);
 
-if (meFresh?.dailyReward?.canClaim) {
-  dailyRewardPopup.show({
-    day: meFresh.dailyReward.day,
-    coins: meFresh.dailyReward.coins,
-    days: meFresh.dailyReward.days,
-    bonusState: meFresh.dailyReward.bonusState,
-  });
-}
+    const meFresh = await apiMe();
+    console.log("WHO OPENED DAILY POPUP", meFresh?.dailyReward);
 
-if (meFresh?.missedDay) {
+    if (meFresh?.dailyReward?.canClaim) {
+      dailyRewardPopup.show({
+        day: meFresh.dailyReward.day,
+        coins: meFresh.dailyReward.coins,
+        days: meFresh.dailyReward.days,
+        bonusState: meFresh.dailyReward.bonusState,
+      });
+    }
 
-  CURRENT_MISSED_DAY = meFresh.missedDay.day;
-  CURRENT_MISSED_COINS = meFresh.missedDay.coins;
+    if (meFresh?.missedDay) {
+      CURRENT_MISSED_DAY = meFresh.missedDay.day;
+      CURRENT_MISSED_COINS = meFresh.missedDay.coins;
+      missedRewardPopup.show(meFresh.missedDay);
+    }
 
-  missedRewardPopup.show(meFresh.missedDay);
-}
-}
-
-if (meFresh?.mysteryChest) {
-  mysteryChestPopup.show();
-}
+    if (meFresh?.mysteryChest) {
+      mysteryChestPopup.show();
+    }
 
     if (RESUME_TILES.size > 0 || RESUME_POS) {
       setTimeout(() => {
